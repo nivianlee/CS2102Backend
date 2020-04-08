9cb8b75dbd783440991ba5dc75068a51583c685b
andlimey <andychan1451@gmail.com>
2020-04-08 16:04:40 +0800

    Add api for customers to view orders

--- index.js
+++ index.js
@@ -1,84 +1,116 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const customers = require('./api/customer/customers');
-const fdsManagers = require('./api/admin/fdsmanagers');
-const restaurants = require('./api/admin/restaurants');
-const restaurantstaff = require('./api/admin/restaurantstaff');
-const foodItems = require('./api/common/fooditems');
-const promotions = require('./api/common/promotions');
+const express = require("express");
+const bodyParser = require("body-parser");
+const customers = require("./api/customer/customers");
+const fdsManagers = require("./api/admin/fdsmanagers");
+const restaurants = require("./api/admin/restaurants");
+const restaurantstaff = require("./api/admin/restaurantstaff");
+const foodItems = require("./api/common/fooditems");
+const promotions = require("./api/common/promotions");
 const app = express();
 const port = 3000;
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
   response.json({
-    info: 'Node.js, Express, and Postgres API'
+    info: "Node.js, Express, and Postgres API",
   });
 });
 
 // customer apis
-app.get('/customers', customers.getCustomers);
-app.get('/customers/:customerid', customers.getCustomerById);
-app.get('/customers/:customerid/addresses', customers.getAddresses);
-app.get('/customers/:customerid/recentaddresses', customers.getRecentAddresses);
-app.get('/customers/:customerid/savedaddresses', customers.getSavedAddresses);
-app.post('/customers', customers.createCustomer);
-app.put('/customers/:customerid', customers.updateCustomer);
-app.delete('/customers/:customerid', customers.deleteCustomer);
+app.get("/customers", customers.getCustomers);
+app.get("/customers/:customerid", customers.getCustomerById);
+app.get("/customers/:customerid/addresses", customers.getAddresses);
+app.get("/customers/:customerid/recentaddresses", customers.getRecentAddresses);
+app.get("/customers/:customerid/savedaddresses", customers.getSavedAddresses);
+app.get("/customers/:customerid/currentorders", customers.getCurrentOrders);
+app.get("/customers/:customerid/pastorders", customers.getPastOrders);
+app.post("/customers", customers.createCustomer);
+app.put("/customers/:customerid", customers.updateCustomer);
+app.delete("/customers/:customerid", customers.deleteCustomer);
 
 // fooditems apis
-app.get('/fooditems', foodItems.getFoodItems);
-app.get('/fooditems/:restaurantid', foodItems.getFoodItemsByRestaurantId);
-app.get('/promotions', promotions.getPromotions);
-app.get('/promotions/:promotionid', promotions.getPromotionsByID);
+app.get("/fooditems", foodItems.getFoodItems);
+app.get("/fooditems/:restaurantid", foodItems.getFoodItemsByRestaurantId);
+app.get("/promotions", promotions.getPromotions);
+app.get("/promotions/:promotionid", promotions.getPromotionsByID);
 
 // admin: fdsManagers
-app.get('/fdsManagers/summaryOne', fdsManagers.getFDSManagerSummaryOne);
-app.get('/fdsManagers/summaryTwo', fdsManagers.getFDSManagerSummaryTwo);
-app.get('/fdsManagers/summaryThree', fdsManagers.getFDSManagerSummaryThree);
-app.get('/fdsManagers/summaryFour', fdsManagers.getFDSManagerSummaryFour);
-app.get('/fdsManagers', fdsManagers.getFDSManagers);
-app.get('/fdsManagers/:managerid', fdsManagers.getFDSManagersById);
-app.post('/fdsManagers', fdsManagers.createFDSManagers);
-app.post('/fdsManagers/:managerid', fdsManagers.updateFDSManagers);
-app.delete('/fdsManagers/:managerid', fdsManagers.deleteFDSManagers);
+app.get("/fdsManagers/summaryOne", fdsManagers.getFDSManagerSummaryOne);
+app.get("/fdsManagers/summaryTwo", fdsManagers.getFDSManagerSummaryTwo);
+app.get("/fdsManagers/summaryThree", fdsManagers.getFDSManagerSummaryThree);
+app.get("/fdsManagers/summaryFour", fdsManagers.getFDSManagerSummaryFour);
+app.get("/fdsManagers", fdsManagers.getFDSManagers);
+app.get("/fdsManagers/:managerid", fdsManagers.getFDSManagersById);
+app.post("/fdsManagers", fdsManagers.createFDSManagers);
+app.post("/fdsManagers/:managerid", fdsManagers.updateFDSManagers);
+app.delete("/fdsManagers/:managerid", fdsManagers.deleteFDSManagers);
 
 // admin: restaurant
-app.get('/restaurants', restaurants.getRestaurants);
-app.get('/restaurants/:restaurantid', restaurants.getRestaurantById);
-app.get('/restaurants/restaurantName/:restaurantname', restaurants.getRestaurantByName);
-app.get('/restaurants/restaurantLocation/:restaurantlocation', restaurants.getRestaurantByLocation);
+app.get("/restaurants", restaurants.getRestaurants);
+app.get("/restaurants/:restaurantid", restaurants.getRestaurantById);
+app.get(
+  "/restaurants/restaurantName/:restaurantname",
+  restaurants.getRestaurantByName
+);
+app.get(
+  "/restaurants/restaurantLocation/:restaurantlocation",
+  restaurants.getRestaurantByLocation
+);
 
 // admin: restaurantStaff
-app.get('/restaurantstaff/orders/:restaurantstaffid', restaurantstaff.getAllCompletedOrders);
-app.get('/restaurantstaff/monthlyOrders/:year/:month/:restaurantstaffid', restaurantstaff.getMonthlyCompletedOrders);
 app.get(
-  '/restaurantstaff/monthlyStatistics/:year/:month/:restaurantstaffid',
+  "/restaurantstaff/orders/:restaurantstaffid",
+  restaurantstaff.getAllCompletedOrders
+);
+app.get(
+  "/restaurantstaff/monthlyOrders/:year/:month/:restaurantstaffid",
+  restaurantstaff.getMonthlyCompletedOrders
+);
+app.get(
+  "/restaurantstaff/monthlyStatistics/:year/:month/:restaurantstaffid",
   restaurantstaff.getMonthlyCompletedOrdersStatistics
 );
 app.get(
-  '/restaurantstaff/monthlyFavourites/:year/:month/:restaurantstaffid',
+  "/restaurantstaff/monthlyFavourites/:year/:month/:restaurantstaffid",
   restaurantstaff.getMonthlyFavouriteFoodItems
 );
-app.get('/restaurantstaff/promotionStatistics/:restaurantstaffid', restaurantstaff.getPromotionalCampaignsStatistics);
-app.post('/restaurantstaff', restaurantstaff.createRestaurantStaff);
-app.post('/restaurantstaff/fooditems/:restaurantstaffid', restaurantstaff.createFoodItem);
-app.put('/restaurantstaff/:restauranttaffid', restaurantstaff.updateRestaurantStaff);
-app.put('/restaurantstaff/fooditems/:restaurantstaffid', restaurantstaff.updateFoodItem);
-app.delete('/restaurantstaff/:restaurantstaffid', restaurantstaff.deleteRestaurantStaff);
+app.get(
+  "/restaurantstaff/promotionStatistics/:restaurantstaffid",
+  restaurantstaff.getPromotionalCampaignsStatistics
+);
+app.post("/restaurantstaff", restaurantstaff.createRestaurantStaff);
+app.post(
+  "/restaurantstaff/fooditems/:restaurantstaffid",
+  restaurantstaff.createFoodItem
+);
+app.put(
+  "/restaurantstaff/:restauranttaffid",
+  restaurantstaff.updateRestaurantStaff
+);
+app.put(
+  "/restaurantstaff/fooditems/:restaurantstaffid",
+  restaurantstaff.updateFoodItem
+);
+app.delete(
+  "/restaurantstaff/:restaurantstaffid",
+  restaurantstaff.deleteRestaurantStaff
+);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
