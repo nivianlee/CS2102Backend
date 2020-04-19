5f5444f397f7cf81449bc1a33cb248fb77414277
Ling Zhi Yu <zhiyulling@gmail.com>
2020-04-19 10:40:11 +0800

    triggers_test.sql: update tests for deferred constraints

--- database/triggers_tests.sql
+++ database/triggers_tests.sql
@@ -33,8 +33,8 @@ BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '10:00:00', '14:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '10:00:00', '14:00:00', 16, 1);
 
     DELETE FROM Riders R
     WHERE R.riderID = 200;
@@ -45,8 +45,8 @@ BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (1, '10:00:00', '15:00:00', 1, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '10:00:00', '15:00:00', 16, 1);
 
     -- Will only run when negative case passes 
     DELETE FROM Riders R
@@ -59,8 +59,8 @@ BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '10:00:00', '14:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '10:00:00', '14:00:00', 16, 1);
 
     DELETE FROM Riders R
     WHERE R.riderID = 200;
@@ -71,11 +71,8 @@ BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '9:00:00', '10:00:00', 4, 1);
-
-    DELETE FROM Riders R
-    WHERE R.riderID = 200;
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '9:00:00', '10:00:00', 16, 1);
 COMMIT;
 
 /* Tests for part_time_riders_schedule_1_hour_break */
@@ -84,11 +81,11 @@ BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '10:00:00', '11:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '10:00:00', '11:00:00', 16, 1);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '12:00:00', '13:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '12:00:00', '13:00:00', 16, 1);
 
     DELETE FROM Riders R
     WHERE R.riderID = 200;
@@ -99,11 +96,11 @@ BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '10:00:00', '11:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '10:00:00', '11:00:00', 16, 1);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '11:00:00', '12:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (200, '11:00:00', '12:00:00', 16, 1);
 
     DELETE FROM Riders R
     WHERE R.riderID = 200;
@@ -111,18 +108,84 @@ COMMIT;
 
 /* Tests for part_time_riders_schedule_between_10_and_48_hours_trigger*/
 -- Positive Test Case: Add interval that satisfies this constraint
-
--- Negative Test Case: Add a lone rider with only 1 schedule entry in a statement 
--- RiderID must be Riders.size + 1 to detect error
 BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (101, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (101, '10:00:00', '11:00:00', 4, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 1);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 1);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 2);
+
+    DELETE FROM Riders R
+    WHERE R.riderID = 101;
 COMMIT;
 
--- Negative Test Case: Remove interval(s) that breaks this constraint 
+-- Negative Test Case: Add a lone rider with only 1 schedule entry in a statement 
+BEGIN;
+    INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
+    VALUES (101, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '11:00:00', 16, 1);
+COMMIT;
+
+-- Negative Test Case: Add a lone rider with  schedule entries > 48 hours 
+BEGIN;
+    SET CONSTRAINTS part_time_riders_schedule_between_10_and_48_hours_trigger DEFERRED;
+
+    INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
+    VALUES (101, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 1);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 1);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 2);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 2);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 3);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 3); 
+    
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 4);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 4);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 5);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 5); 
+    
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 6);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 6);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 7);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 7); 
+COMMIT;
+
+
+-- -- Negative Test Case: Remove interval(s) that breaks this constraint 
 -- Remove all entries; must implement check on delete
 BEGIN;
     DELETE FROM PartTimeSchedules P 
@@ -132,24 +195,43 @@ COMMIT;
 /* Tests for part_time_riders_schedule_start_and_end_on_hour_trigger */
 -- Positive Test Case: Add interval that starts and ends on the hour 
 BEGIN;
-    INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
-    VALUES (200, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
+    SET CONSTRAINTS part_time_riders_schedule_start_and_end_on_hour_trigger DEFERRED;
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (200, '10:00:00', '11:00:00', 4, 1);
-
-    DELETE FROM Riders R
-    WHERE R.riderID = 200;
-COMMIT;
-
--- Negative Test Case: Add interval that does not start and end on the hour
--- RiderID must be Riders.size + 1 to detect error
-BEGIN;
     INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
     VALUES (101, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
 
-    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, duration, day)
-    VALUES (101, '10:00:00', '11:30:00', 4, 1);
+    -- For a valid interval schedule > 10 hours 
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 1); -- overlapped by insertion below 
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 2);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 2); -- overlaps
+
+    DELETE FROM Riders R
+    WHERE R.riderID = 101;
+COMMIT;
+
+-- Negative Test Case: Add interval that does not start and end on the hour
+BEGIN;
+    SET CONSTRAINTS part_time_riders_schedule_start_and_end_on_hour_trigger DEFERRED;
+
+    INSERT INTO Riders(riderID, riderName, riderEmail, contactNum, isOccupied, isFullTime, baseSalary)
+    VALUES (101, 'Dummy', 'dummy@dummymail.com', '12345678', TRUE, TRUE, 123);
+
+    -- For a valid interval schedule > 10 hours 
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 1); -- overlapped by insertion below 
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '15:00:00', '19:00:00', 16, 1);
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '14:00:00', 16, 2);
+
+    INSERT INTO PartTimeSchedules(riderID, startTime, endTime, week, day)
+    VALUES (101, '10:00:00', '11:00:00', 16, 1); -- overlaps
 
     DELETE FROM Riders R
     WHERE R.riderID = 101;
@@ -157,13 +239,15 @@ COMMIT;
 
 /* Tests for part_time_riders_participation_in_hour_interval_trigger and full_time_riders_participation_in_hour_interval_trigger*/
 -- Positive Test Case: Add interval for part time rider such that each hour interval still has 5 riders
--- Won't throw exception if recordss already have 5 riders 
+-- Won't throw exception if records already have 5 riders per hour interval 
 
 -- Negative Test Case: Remove interval for both part time and full time riders that results in hour interval having less than 5 riders 
 BEGIN;
     DELETE FROM PartTimeSchedules P 
-    WHERE P.startTime = '10:00:00'::TIME;
+    WHERE P.startTime = '10:00:00'::TIME; -- delete all schedules at '10:00:00'
+COMMIT;
 
-    DELETE FROM FullTimeSchedules F 
-    WHERE F.startTime = '10:00:00'::TIME;
+BEGIN;
+    DELETE FROM FullTimeSchedules F -- delete all from shift
+    WHERE shiftID <> 1;
 COMMIT;
