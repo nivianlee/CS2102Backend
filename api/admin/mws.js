cc75a930d819a463135ebe4df4c98895e4afe2ef
Niv <nivian.lee@gmail.com>
2020-05-07 13:15:45 +0800

    Edited mws and promotions api

--- api/admin/mws.js
+++ api/admin/mws.js
@@ -1,5 +1,15 @@
 const pool = require('../../pool.js');
 
+const getShiftsTable = (request, response) => {
+  const query = `SELECT * FROM shifts ORDER BY shiftid ASC;`;
+  pool.query(query, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
 const getMwsFullTimeRiders = (request, response) => {
   const query = `
     SELECT * 
@@ -7,7 +17,7 @@ const getMwsFullTimeRiders = (request, response) => {
           JOIN Shifts USING(shiftID)
           JOIN DayRanges USING(rangeID)
     ;
-  `
+  `;
   pool.query(query, (error, results) => {
     if (error) {
       throw error;
@@ -24,8 +34,8 @@ const getMwsFullTimeRiderById = (request, response) => {
           JOIN DayRanges USING(rangeID)
     WHERE riderID = $1 
     ;
-  `
-  const riderid = parseInt(request.params.riderid)
+  `;
+  const riderid = parseInt(request.params.riderid);
   pool.query(query, [riderid], (error, results) => {
     if (error) {
       throw error;
@@ -43,9 +53,9 @@ const getMwsFullTimeRiderByMonth = (request, response) => {
     WHERE riderID = $1 
     AND month = $2
     ;
-  `
-  const riderid = parseInt(request.params.riderid)
-  const monthid = parseInt(request.params.monthid)
+  `;
+  const riderid = parseInt(request.params.riderid);
+  const monthid = parseInt(request.params.monthid);
   pool.query(query, [riderid, monthid], (error, results) => {
     if (error) {
       throw error;
@@ -59,7 +69,7 @@ const getWwsPartTimeRiders = (request, response) => {
     SELECT * 
     FROM PartTimeSchedules  
     ;
-  `
+  `;
   pool.query(query, (error, results) => {
     if (error) {
       throw error;
@@ -74,8 +84,8 @@ const getWwsPartTimeRiderById = (request, response) => {
     FROM PartTimeSchedules  
     WHERE riderID = $1 
     ;
-  `
-  const riderid = parseInt(request.params.riderid)
+  `;
+  const riderid = parseInt(request.params.riderid);
 
   pool.query(query, [riderid], (error, results) => {
     if (error) {
@@ -92,9 +102,9 @@ const getWwsPartTimeRiderByMonth = (request, response) => {
     WHERE riderID = $1 
     AND CEILING(week / 4 ::float) = $2
     ;
-  `
-  const riderid = parseInt(request.params.riderid)
-  const monthid = parseInt(request.params.monthid)
+  `;
+  const riderid = parseInt(request.params.riderid);
+  const monthid = parseInt(request.params.monthid);
 
   pool.query(query, [riderid, monthid], (error, results) => {
     if (error) {
@@ -109,13 +119,13 @@ const createMwsFullTimeRider = (request, response) => {
   const query = `INSERT INTO FullTimeSchedules(riderID, shiftID, rangeID, month)
                  VALUES ($1, $2, $3, $4) 
                  RETURNING *
-  `
+  `;
   const data = {
     riderid: request.body.riderid,
     shiftid: request.body.shiftid,
     rangeid: request.body.rangeid,
-    monthid: request.body.monthid
-  }
+    month: request.body.month,
+  };
   pool.query(query, Object.values(data), (error, results) => {
     if (error) {
       throw error;
@@ -127,23 +137,28 @@ const createMwsFullTimeRider = (request, response) => {
 // Precondition: All arrays are of equal length
 const createWwsPartTimeRider = (request, response) => {
   const query = `INSERT INTO PartTimeSchedules (riderID,startTime,endTime,week,day) 
-  VALUES ($1, $2, $3, $4, $5) RETURNING *`
+  VALUES ($1, $2, $3, $4, $5) RETURNING *`;
   const data = {
     rideridArr: request.body.rideridArr,
     startTimeArr: request.body.startTimeArr,
     endTimeArr: request.body.endTimeArr,
     weekArr: request.body.weekArr,
-    dayArr: request.body.dayArr
+    dayArr: request.body.dayArr,
   };
   (async () => {
-    const client = await pool.connect()
+    const client = await pool.connect();
 
     try {
-      await client.query('BEGIN')
-      for (let i = 0; i < data['rideridArr'].length; i++) { // iterate through all arrays 
-        const values = [data['rideridArr'][i], data['startTimeArr'][i],
-          data['endTimeArr'][i], data['weekArr'][i], data['dayArr'][i]
-        ]
+      await client.query('BEGIN');
+      for (let i = 0; i < data['rideridArr'].length; i++) {
+        // iterate through all arrays
+        const values = [
+          data['rideridArr'][i],
+          data['startTimeArr'][i],
+          data['endTimeArr'][i],
+          data['weekArr'][i],
+          data['dayArr'][i],
+        ];
         await client.query(query, values, (error, results) => {
           if (error) {
             throw error;
@@ -152,84 +167,80 @@ const createWwsPartTimeRider = (request, response) => {
       }
       await client.query('COMMIT', (error, results) => {
         const msg = {
-          "success": "True",
-          "message": `Successful creation of values in PartTimeSchedules with the given values`,
-          "data": data
-        }
+          success: 'True',
+          message: `Successful creation of values in PartTimeSchedules with the given values`,
+          data: data,
+        };
         response.status(201).send(msg);
-      })
+      });
     } catch (e) {
-      await client.query('ROLLBACK')
-      throw e
+      await client.query('ROLLBACK');
+      throw e;
     } finally {
-      client.release()
+      client.release();
     }
-  })().catch(e => console.error(e.stack))
+  })().catch((e) => console.error(e.stack));
 };
 
 const updateMwsFullTimeRider = (request, response) => {
   const query = `UPDATE FullTimeSchedules SET riderid = $1, 
   shiftID = $2, rangeID = $3, month = $4
   RETURNING *
-  ;`
+  ;`;
 
   const data = {
-    riderid: parseInt(request.body.riderid),
-    shiftid: parseInt(request.body.shiftid),
-    rangeid: parseInt(request.body.rangeid),
-    month: parseInt(request.body.month)
+    riderid: request.body.riderid,
+    shiftid: request.body.shiftid,
+    rangeid: request.body.rangeid,
+    month: request.body.month,
   };
 
-  pool.query(query, Object.values(data),
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).send({
-        message: "Succesful update of MWS for FullTime rider!"
-      });
+  pool.query(query, Object.values(data), (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    response.status(201).send({
+      message: 'Succesful update of MWS for FullTime rider!',
+    });
+  });
 };
 
-// Updates a MWS row for a FullTime rider 
+// Updates a MWS row for a FullTime rider
 const updateWwsPartTimeRider = (request, response) => {
   const query = `UPDATE FullTimeSchedules 
                  SET shiftID = $2, rangeID = $3 
                  WHERE riderid = $1
                  AND month = $4
                  RETURNING *
-                 ;`
+                 ;`;
 
   const data = {
     riderid: request.body.riderid,
     shiftid: request.body.shiftid,
     rangeid: request.body.rangeid,
-    month: request.body.month
+    month: request.body.month,
   };
 
-  pool.query(query, Object.values(data),
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).send({
-        message: "Succesful update of MWS for FullTime rider!",
-        "results": results.rows
-      });
+  pool.query(query, Object.values(data), (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    response.status(201).send({
+      message: 'Succesful update of MWS for FullTime rider!',
+      results: results.rows,
+    });
+  });
 };
 
-// // Updates a WWS time period for a Part Time rider 
-// No update WWS for partTime rider: To uniquely identify a schedule for a row, must use all attributes. 
-// Easier to use delete and create apis. 
+// // Updates a WWS time period for a Part Time rider
+// No update WWS for partTime rider: To uniquely identify a schedule for a row, must use all attributes.
+// Easier to use delete and create apis.
 
 const deleteMwsFullTimeRiderByMonth = (request, response) => {
   const query = `DELETE FROM FullTimeSchedules 
                  WHERE riderID = $1
                  AND month = $2
-                 RETURNING *`
+                 RETURNING *`;
   const data = {
     riderid: request.body.riderid,
     monthid: request.body.monthid,
@@ -248,26 +259,27 @@ const deleteWwsPartTimeRiderByWeek = (request, response) => {
                  AND startTime = $2
                  AND endTime = $3
                  AND week = $4
-                 AND day = $5`
+                 AND day = $5`;
   const data = {
     riderid: parseInt(request.body.riderid),
     startTime: request.body.startTime,
     endTime: request.body.endTime,
     week: request.body.week,
-    day: request.body.day
-  }
+    day: request.body.day,
+  };
 
   pool.query(query, Object.values(data), (error, results) => {
     if (error) {
       throw error;
     }
     response.status(201).send({
-      message: `Rider with riderID = ${data.riderid} at ${data.startTime} to ${data.endTime} on Week ${data.week} and Day ${data.day} has been deleted`
+      message: `Rider with riderID = ${data.riderid} at ${data.startTime} to ${data.endTime} on Week ${data.week} and Day ${data.day} has been deleted`,
     });
   });
 };
 
 module.exports = {
+  getShiftsTable,
   getMwsFullTimeRiders,
   getMwsFullTimeRiderByMonth,
   getMwsFullTimeRiderById,
@@ -279,5 +291,5 @@ module.exports = {
   updateMwsFullTimeRider,
   updateWwsPartTimeRider,
   deleteMwsFullTimeRiderByMonth,
-  deleteWwsPartTimeRiderByWeek
+  deleteWwsPartTimeRiderByWeek,
 };
